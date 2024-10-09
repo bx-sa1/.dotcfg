0592d9c8e7fff59b4afbea2ae30dabe5b484b7d7
Baba <bms110403@gmail.com>
2024-10-09 17:10:49 +0100

    oct9

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -5,8 +5,8 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
-
-
+import XMonad.Layout.Spacing
+import XMonad.Layout.SimpleDecoration
 
 main :: IO ()
 main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ myConfig
@@ -16,7 +16,20 @@ myConfig = def
   , terminal = "alacritty"
   , startupHook = myStartupHook
   , workspaces = myWorkspaces
-  } `additionalKeysP` myKeyMap 
+  , manageHook = myManageHook
+  , layoutHook = spacingWithEdge 10 $ myLayoutHook
+  } 
+  `additionalKeysP`
+  [ ("M-r", spawn "rofi -show combi -combi-modes drun,run,windows")
+  , ("M-S-q", spawn "quit_rofi")
+  ]
+  `remapKeysP` 
+  [ ("M-w", "M-S-c") 
+  ]
+  `removeKeysP`
+  [ "M-p"
+  ]
+  
 
 myWorkspaces = ["home","web","dev","music","games"]
 myModMask = mod4Mask
@@ -25,14 +38,10 @@ myStartupHook :: X ()
 myStartupHook = do
   spawnOnce "nitrogen --restore"
   spawnOnce "dunst"
-  spawnOnce "polybar"
+  spawnOnce "polybar xmonad"
   spawnOnce "picom -b"
   spawnOnce "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
   spawnOnce "dex -a -w"
-
-myKeyMap = 
-  [ ("M-p", spawn "rofi -show combi -combi-modes drun,run,windows")
-  ]
 
 myManageHook :: ManageHook
 myManageHook = composeAll
@@ -42,3 +51,11 @@ myManageHook = composeAll
   , className =? "Discord" --> doShift "games"
   , className =? "REAPER" --> doShift "music"
   ]
+
+myLayoutHook = tiled ||| Mirror tiled ||| Full
+  where
+    tiled   = Tall nmaster delta ratio
+    nmaster = 1      -- Default number of windows in the master pane
+    ratio   = 1/2    -- Default proportion of screen occupied by master pane
+    delta   = 3/100  -- Percent of screen to increment by when resizing panes
+
