9dfbb632af6fdbe4cb840dd08b9a6e191dabe6de
Baba <bms110403@gmail.com>
2024-10-09 10:35:03 +0100

    xmonad

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,8 +1,44 @@
 import XMonad
-import XMonad.Config.Desktop
+import XMonad.Util.EZConfig
+import XMonad.Util.SpawnOnce
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+
 
 main :: IO ()
-main = xmonad $ desktopConfig
-  { modMask = mod4Mask
+main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ myConfig
+
+myConfig = def
+  { modMask = myModMask
   , terminal = "alacritty"
-  }
+  , startupHook = myStartupHook
+  , workspaces = myWorkspaces
+  } `additionalKeysP` myKeyMap 
+
+myWorkspaces = ["home","web","dev","music","games"]
+myModMask = mod4Mask
+
+myStartupHook :: X ()
+myStartupHook = do
+  spawnOnce "nitrogen --restore"
+  spawnOnce "dunst"
+  spawnOnce "polybar"
+  spawnOnce "picom -b"
+  spawnOnce "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+  spawnOnce "dex -a -w"
+
+myKeyMap = 
+  [ ("M-p", spawn "rofi -show combi -combi-modes drun,run,windows")
+  ]
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+  [ className =? "firefox" --> doShift "web"
+  , className =? "steam" --> doShift "games"
+  , className =? "Lutris" --> doShift "games"
+  , className =? "Discord" --> doShift "games"
+  , className =? "REAPER" --> doShift "music"
+  ]
