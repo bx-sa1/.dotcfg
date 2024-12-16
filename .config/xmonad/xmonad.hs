418692a408413e27cfa2672356ed0e9b3596a03f
bx_sa1 <bms110403@gmail.com>
2024-12-16 14:51:01 +0000

    2024-12-16

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -5,6 +5,7 @@ import System.FilePath ((</>))
 import XMonad
 import XMonad.Actions.CopyWindow (copy, copyToAll, killAllOtherCopies)
 import XMonad.Actions.DynamicWorkspaces
+import XMonad.Actions.UpdateFocus
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
@@ -12,15 +13,20 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.Place
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
+import XMonad.Layout.FocusTracking
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
 import XMonad.Layout.SimpleDecoration
 import XMonad.Layout.Spacing
+import XMonad.Layout.Tabbed
+import XMonad.Layout.TrackFloating
 import XMonad.StackSet (lookupWorkspace)
 import XMonad.StackSet qualified as W
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
+
+isXfce = className ^? "Xfce4"
 
 loadColors :: IO [String]
 loadColors = do
@@ -32,112 +38,121 @@ loadColors = do
         Right f -> take 8 $ lines f
   return colors
 
-main :: IO ()
-main = do
-  safeSpawn "mkfifo" ["/tmp/xmonad-polybar"]
-  handle <- spawnPipe "stdbuf -i 0 tee /tmp/xmonad-polybar"
-  colors <- loadColors
-  xmonad $ ewmhFullscreen . ewmh . docks $ myConfig handle colors
-
 myModMask = mod4Mask
 
-myConfig handle colors =
-  def
-    { modMask = mod4Mask,
-      normalBorderColor = colors !! 1,
-      focusedBorderColor = colors !! 5,
-      terminal = "alacritty",
-      workspaces = ["home", "web", "dev", "music", "games"],
-      startupHook = myStartupHook,
-      manageHook = myManageHook,
-      layoutHook = myLayoutHook,
-      logHook = myPP
-    }
-    `additionalKeysP` addKeysP
-    `removeKeysP` delKeysP
-    `additionalKeys` addKeys
+myStartupHook = do
+  spawnOnce "picom -b"
+
+myLayoutHook =
+  focusTracking $
+    smartBorders $
+      spacingWithEdge 10 $
+        avoidStruts $
+          tiled ||| Mirror tiled ||| simpleTabbed
   where
     tiled = Tall nmaster delta ratio
     nmaster = 1 -- Default number of windows in the master pane
     ratio = 1 / 2 -- Default proportion of screen occupied by master pane
     delta = 3 / 100 -- Percent of screen to increment by when resizing panes
-    myLayoutHook =
-      avoidStruts $
-        smartBorders $
-          spacingWithEdge 10 $
-            tiled ||| Mirror tiled ||| Full
 
-    myStartupHook = do
-      spawnOnce "picom -b"
+myManageHook =
+  placeHook (smart (0.5, 0.5))
+    <> composedManageHook
+    <> manageHook def
+  where
+    cShifts =
+      [ ("firefox", "web"),
+        ("steam", "games"),
+        ("Lutris", "games"),
+        ("discord", "games"),
+        ("REAPER", "music"),
+        ("com.bitwig.BitwigStudio", "music"),
+        ("Qtractor", "music"),
+        ("Spotify", "music")
+      ]
 
-    isXfce = className ^? "Xfce4"
+    cFloats =
+      [ "Localsend",
+        "pavucontrol",
+        "Anki",
+        "Pcmanfm",
+        "Alacritty",
+        "Thunar"
+      ]
+
     composedManageHook =
-      composeAll
-        [ className =? "firefox" --> doShift "web",
-          className =? "steam" --> doShift "games",
-          className =? "Lutris" --> doShift "games",
-          className =? "discord" --> doShift "games",
-          className =? "REAPER" --> doShift "music",
-          className =? "com.bitwig.BitwigStudio" --> doShift "music",
-          className =? "Qtractor" --> doShift "music",
-          className =? "Spotify" --> doShift "music",
-          className =? "Pcmanfm" --> doFloat,
-          className =? "Alacritty" --> doFloat,
-          className =? "firefox" <&&> appName =? "Toolkit" --> doFloat,
-          className =? "Localsend" --> doFloat,
-          className =? "pavucontrol" --> doFloat,
-          className =? "Anki" --> doFloat,
-          checkDock --> doIgnore <+> doRaise,
-          isNotification --> doIgnore <+> doRaise,
-          isDialog --> doFloat,
-          isXfce --> doFloat,
-          transience'
+      composeOne . concat $
+        [ [className =? c -?> doShift ws | (c, ws) <- cShifts],
+          [className =? c -?> doFloat | c <- cFloats],
+          [className =? "firefox" <&&> appName =? "Toolkit" -?> doFloat],
+          [checkDock -?> doRaise],
+          [isNotification -?> doIgnore <+> doRaise],
+          [isDialog -?> doFloat],
+          [isXfce -?> doFloat],
+          [transience]
         ]
-    myManageHook =
-      placeHook (smart (0.5, 0.5))
-        <> manageDocks
-          <+> manageHook def
-          <+> composedManageHook
 
-    myPP =
-      dynamicLogWithPP $
-        def
-          { ppOutput = hPutStrLn handle,
-            ppOrder = \(_ : l : _ : _) -> [l]
-          }
+myPP handle =
+  dynamicLogWithPP $
+    def
+      { ppOutput = hPutStrLn handle,
+        ppOrder = \(_ : l : _ : _) -> [l]
+      }
 
-    addKeysP =
-      [ ("M-w", kill),
-        ("M-r", spawn "rofi -show combi -combi-modes drun,run,windows"),
-        ("M-S-q", spawn "quit_rofi"),
-        ("M-=", addWorkspacePrompt def),
-        ("M--", removeWorkspace),
-        ("M-\\ S-b", spawn "toggle-replay-buffer.sh"),
-        ("M-\\ b", spawn "save-replay.sh"),
-        ("<Print>", spawn "flameshot gui"),
-        ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 5%+"),
-        ("<XF86AudioLowerVolume>", spawn "amixer sset Master 5%-"),
-        ("<XF86AudioMute>", spawn "amixer sset Master 1+ toggle"),
-        ("<XF86AudioPlay>", spawn "playerctl play-pause"),
-        ("<XF86AudioNext>", spawn "playerctl next"),
-        ("<XF86AudioPrev>", spawn "playerctl previous"),
-        ("M-v", spawn "clipcat-menu && sleep 1"),
-        ("M-S-v", spawn "clipcat-menu && xdotool type \"$(xlip -out -selection clipboard\")"),
-        ("M-s", windows copyToAll),
-        ("M-S-s", killAllOtherCopies)
-      ]
+addKeysP =
+  [ ("M-w", kill),
+    ("M-r", spawn "rofi -show combi -combi-modes drun,run,windows"),
+    ("M-S-q", spawn "quit_rofi"),
+    ("M-=", addWorkspacePrompt def),
+    ("M--", removeWorkspace),
+    ("M-\\ S-b", spawn "toggle-replay-buffer.sh"),
+    ("M-\\ b", spawn "save-replay.sh"),
+    ("<Print>", spawn "flameshot gui"),
+    ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 5%+"),
+    ("<XF86AudioLowerVolume>", spawn "amixer sset Master 5%-"),
+    ("<XF86AudioMute>", spawn "amixer sset Master 1+ toggle"),
+    ("<XF86AudioPlay>", spawn "playerctl play-pause"),
+    ("<XF86AudioNext>", spawn "playerctl next"),
+    ("<XF86AudioPrev>", spawn "playerctl previous"),
+    ("M-v", spawn "clipcat-menu && sleep 1"),
+    ("M-S-v", spawn "clipcat-menu && xdotool type \"$(xlip -out -selection clipboard\")"),
+    ("M-s", windows copyToAll),
+    ("M-S-s", killAllOtherCopies),
+    ("M-b", sendMessage ToggleStruts)
+  ]
 
-    delKeysP =
-      [ "M-p",
-        "M-S-c"
-      ]
+delKeysP =
+  [ "M-p",
+    "M-S-c"
+  ]
 
-    addKeys =
-      [ ((myModMask .|. m, kc), withNthWorkspace f w)
-        | (kc, w) <- zip ([xK_1 .. xK_9] ++ [xK_0]) [0 ..],
-          (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
-      ]
-        ++ [ ((myModMask .|. m, kc), screenWorkspace sc >>= flip whenJust (windows . f))
-             | (kc, sc) <- zip ([xK_1 .. xK_9] ++ [xK_0]) [0 ..],
-               (f, m) <- [(W.view, mod1Mask), (W.shift, mod1Mask .|. shiftMask)]
-           ]
+addKeys =
+  [ ((myModMask .|. m, kc), withNthWorkspace f w)
+    | (kc, w) <- zip ([xK_1 .. xK_9] ++ [xK_0]) [0 ..],
+      (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+  ]
+    ++ [ ((myModMask .|. m, kc), screenWorkspace sc >>= flip whenJust (windows . f))
+         | (kc, sc) <- zip ([xK_1 .. xK_9] ++ [xK_0]) [0 ..],
+           (f, m) <- [(W.view, mod1Mask), (W.shift, mod1Mask .|. shiftMask)]
+       ]
+
+main :: IO ()
+main = do
+  safeSpawn "mkfifo" ["/tmp/xmonad-polybar"]
+  handle <- spawnPipe "stdbuf -i 0 tee /tmp/xmonad-polybar"
+  colors <- loadColors
+  xmonad . ewmhFullscreen . ewmh . docks $
+    def
+      { modMask = myModMask,
+        normalBorderColor = colors !! 1,
+        focusedBorderColor = colors !! 5,
+        terminal = "alacritty",
+        workspaces = ["home", "web", "dev", "music", "games"],
+        startupHook = myStartupHook,
+        manageHook = myManageHook,
+        layoutHook = myLayoutHook,
+        logHook = myPP handle
+      }
+      `additionalKeysP` addKeysP
+      `removeKeysP` delKeysP
+      `additionalKeys` addKeys
