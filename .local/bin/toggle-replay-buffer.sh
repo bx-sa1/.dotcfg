6328c410c4b37a70397333bfa9b3d95d29664507
bx_sa1 <bms110403@gmail.com>
2025-03-17 14:00:01 +0000

    walls

--- .local/bin/toggle-replay-buffer.sh
+++ .local/bin/toggle-replay-buffer.sh
@@ -8,5 +8,5 @@ if pidof -q gpu-screen-recorder; then
   killall -SIGINT gpu-screen-recorder
 else
   notify-send "gpu-screen-recorder" "Starting Replay Buffer..."
-  gpu-screen-recorder -w screen -f 60 -a "$(pactl get-default-sink).monitor" -a "$(pactl get-default-source)" -c mkv -bm cbr -r 30 -o "$video_path"
+  gpu-screen-recorder -w screen -f 60 -a "$(pactl get-default-sink).monitor" -a "$(pactl get-default-source)" -c mkv -r 30 -o "$video_path"
 fi
