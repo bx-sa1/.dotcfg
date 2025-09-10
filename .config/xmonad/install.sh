2383380d7a41acb60156402dadb42f7e8a563ff6
bx_sa1 <bms110403@gmail.com>
2025-09-10 21:10:54 +0100

    deleting xmonad

--- .config/xmonad/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-cabal update
-cabal install --package-env=$HOME/.config/xmonad --lib base xmonad xmonad-contrib  -j
-cabal install --package-env=$HOME/.config/xmonad xmonad --overwrite-policy=always -j
