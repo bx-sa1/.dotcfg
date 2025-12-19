8636322a4eca59272c7deb5f4918773a3eddd205
bx_sa1 <bms110403@gmail.com>
2025-12-19 02:20:39 +0000

    changes

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,9 +1,14 @@
+from qutebrowser.config.configfiles import ConfigAPI
+from qutebrowser.config.config import ConfigContainer
+config: ConfigAPI = config  # pyright: ignore
+c: ConfigContainer = c  # pyright: ignore
+
 config.source('colors.py')
 
 c.editor.command = ['emacsclient', '-c', '-n', '-a ', '+{line0}:{column0}', '{file}' ]
 
-config.bind(',V', 'hint links spawn mpv {hint-url}')
-config.bind(',v', 'spawn mpv {url}')
+config.bind(',V', 'hint links spawn python /usr/share/mpv/scripts/umpv {hint-url}')
+config.bind(',v', 'spawn python /usr/share/mpv/scripts/umpv {url}')
 config.bind('zl', 'spawn --userscript qute-pass --mode gopass')
 config.bind('zol', 'spawn --userscript qute-pass --mode gopass --otp-only')
 config.bind('zpl', 'spawn --userscript qute-pass --mode gopass --password-only')
